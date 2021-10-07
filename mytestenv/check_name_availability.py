1ada73a12638bc9a85296761c2b41a4e8ba90444
Chad Carroll <ccarroll@microsoft.com>
2021-10-07 18:09:17 +0000

    initial commit

---
+++ mytestenv/check_name_availability.py
@@ -0,0 +1,23 @@
+#create class
+
+# Storage Account
+def storage (storage_client, storage_account_name):
+    #storage_account_name = 'invalid-or-used-name'
+    availability = storage_client.storage_accounts.check_name_availability(storage_account_name)
+    print('The storage account account {} is available: {}'.format(storage_account_name, availability.name_available))
+    print('Reason: {}'.format(availability.reason))
+
+def key_vault (key_vault_client, RESOURCE_GROUP, key_vault_name):
+    if key_vault_client.resources.check_existence(RESOURCE_GROUP, 'Microsoft.KeyVault',
+                                  '',
+                                  'vaults',
+                                  key_vault_name,
+                                  '2021-04-01'):
+        print (f"Key Vault {key_vault_name} exits ")
+    else:
+        print (f"Key Vault {key_vault_name} NOT exits ")
+
+
+# keyvault base name check
+# 23 characters or less
+# no more than 1 hypen
--- mytestenv/somefile.py
+++ mytestenv/somefile.py
@@ -3,6 +3,14 @@ import os
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
+from azure.mgmt.keyvault import KeyVaultManagementClient
+from azure.mgmt.storage.models import (
+    StorageAccountCreateParameters,
+    StorageAccountUpdateParameters,
+    Sku,
+    SkuName,
+    Kind
+)
 
 subscription_id="0a7ac580-9e64-426b-a0bd-6cd969e735c3"
 AZURE_TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47"
@@ -23,5 +31,50 @@ credential = AzureCliCredential()
 
 resource_client = ResourceManagementClient(credential, subscription_id)
 storage_client = StorageManagementClient(credential, subscription_id)
+keyvault_client = KeyVaultManagementClient(credential, subscription_id)
 
-# check_name_availability.storage('cstorageaccount10')
+storage_account_name='cstorageaccount11'
+resource_group_name='python'
+
+storage_accounts = ['ccstorage001', 'ccstorage002', 'ccstorage003']
+
+# for sa in storage_accounts:
+#     storage_client.storage_accounts.begin_create(
+#         resource_group_name,
+#         sa,
+#         StorageAccountCreateParameters(
+#             sku=Sku(name=SkuName.standard_ragrs),
+#             kind=Kind.storage,
+#             location='eastus2',
+#             enable_https_traffic_only=True
+#         )
+#     )
+
+poller = keyvault_client.vaults.begin_create_or_update(
+    resource_group_name,
+    'testvaultacb19',
+    {
+        'location': 'eastus2',
+        'properties': {
+            'sku': {
+                'family': 'A',
+                'name':'standard'
+            },
+            'tenantId': AZURE_TENANT_ID,
+            'enabledForDeployment':'true',
+            'enableRbacAuthorization':'true'
+        }
+    }
+)
+
+kv_result = poller.result()
+
+print(f"Provisioned key vault {kv_result.name} in location {kv_result.location}")
+
+keyvault_client.vaults.delete(resource_group_name,'testvaultacb19')
+
+# storage_client.storage_accounts.check_name_availability(storage_account_name)
+
+# storage_client.storage_accounts.check_name_availability()
+
+# check_name_availability.storage(storage_client, 'cstorageaccount10')
